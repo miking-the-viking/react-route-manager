e170e24bf8030ed4039ea8ed22f024949195729d
miking-the-viking <kingmi123@gmail.com>
2020-11-27 13:49:03 -0500

    Scaffolding NX

--- libs/route-manager/src/lib/state.ts
+++
@@ -1,7 +0,0 @@
-import { EvaluatedRouteManagerRoute, RouteManagerRouteConfig } from './types';
-
-export interface RouteManagerState<i extends {}> {
-    coreRoutes: RouteManagerRouteConfig<i>[];
-    setCoreRoutes: (coreRoute: RouteManagerRouteConfig<i>[]) => void;
-    evaluatedRoutes: EvaluatedRouteManagerRoute<i>[];
-}
